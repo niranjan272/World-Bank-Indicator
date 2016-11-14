b679e2000c53b8ad9f69b52ad55281ab4ed29270
niranjan272 <ndeshpande87@yahoo.com>
2016-11-14 15:22:56 -0600

    Changes

--- wdi_data_pivot.R
+++ wdi_data_pivot.R
@@ -38,3 +38,6 @@ agricultural_land_clustering$cluster <- as.factor(agricultural_land_clustering$c
 clusplot(agricultural_land[,4:5],agricultural_land_clustering$cluster,colour = T,shade = T)
 plotcluster(wdi_gini_index[,3:4],wdi_gini_index_clustering$cluster)
 plot(wdi_gini_index$Year, wdi_gini_index$GINI.index..World.Bank.estimate.,col=wdi_gini_index_clustering$cluster)
+
+
+#Ggplot
