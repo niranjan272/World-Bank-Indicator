4bd3db66c49ac95b69ad9280b021e1a8e57c7d1b
niranjan272 <ndeshpande87@yahoo.com>
2016-11-16 14:05:26 -0600

    Code Changes

--- wdi_data_pivot.R
+++ wdi_data_pivot.R
@@ -1,9 +1,10 @@
 setwd("E:\\Fall'16\\03 BUAN 6356\\Project\\Codes\\World-Bank-Indicator\\")
 library(ggplot2)
 library(cluster)
+library(plyr)
 library(fpc)
 #Load File
-indicator_pivot  <- read.csv("..\\..\\world-development-indicators\\indiacator_pivot_names.csv")
+indicator_pivot  <- read.csv("..\\..\\world-development-indicators\\indiacator_pivot_names.csv",na.strings = "..")
 
 #Load Series data
 data_series <- read.csv("..\\..\\world-development-indicators\\Series.csv")
@@ -25,6 +26,7 @@ ncol(indicator_pivot_continent)
 #Reordering Columns
 indicator_pivot_continent <- indicator_pivot_continent[,c(1348,1:1347)]
 colnames(indicator_pivot_continent)[c(1,2,3,4,5)]
+count(indicator_pivot_continent,"Continent")
 
 #Agricultural land (% of land area)
 agricultural_land <- subset(indicator_pivot_continent[c(1,2,3,4,9)])
@@ -37,7 +39,48 @@ agricultural_land_clustering$cluster <- as.factor(agricultural_land_clustering$c
 #Plot Cluster Data
 clusplot(agricultural_land[,4:5],agricultural_land_clustering$cluster,colour = T,shade = T)
 plotcluster(wdi_gini_index[,3:4],wdi_gini_index_clustering$cluster)
-plot(wdi_gini_index$Year, wdi_gini_index$GINI.index..World.Bank.estimate.,col=wdi_gini_index_clustering$cluster)
+plot(wdi_gini_index$Year, wdi_gini_index$G7INI.index..World.Bank.estimate.,col=wdi_gini_index_clustering$cluster)
 
 
-#Ggplot
+
+#Electricity Consuption
+# % of Population
+electricity_access <- subset(indicator_pivot_continent[c(1,2,3,4,352,353,354)], 
+                             Year == 1990 | Year == 2000 | Year == 2010 | Year == 2012 )
+colnames(electricity_access)
+count(electricity_access,"Year")
+ggplot(electricity_access, aes(x = electricity_access[,7], y = factor(Year),colour = electricity_access$Continent)) + 
+  geom_point() + xlab("Access to electricity (% of population)") + ylab("Year")
+
+#Rular Population
+ggplot(electricity_access, aes(x = electricity_access[,5], y = factor(Year),colour = electricity_access$Continent)) + 
+  geom_point() + xlab("Access to electricity (% of rural population)") + ylab("Year")
+
+#Urban Population
+ggplot(electricity_access, aes(x = electricity_access[,6], y = factor(Year),colour = electricity_access$Continent)) + 
+  geom_point() + xlab("Access to electricity (% of urban population)") + ylab("Year")
+
+#Electricity Consumption Clustering
+list_country  = c('AFG' ,	'ARG' ,	'BRA' , 	'CHI' ,	'ETH' ,	'DEU' ,	'IND' ,	'PAK' ,	'RUS' ,	'THA' ,	'GBR' , 'USA')
+list_year  = c('1990','2000','2010','2012')
+electricty_access_country_year <- subset(electricity_access, CountryCode %in% list_country & Year  %in% list_year)
+head(electricty_access_country_year,5)
+electricity_access_clustering <- kmeans(electricty_access_country_year[,c(4,7)],4,nstart = 20)
+
+#clusplot(electricty_access_country_year[,c(4,7)],electricity_access_clustering$cluster,colour = T,shade = T)
+center_cluster = as.data.frame(electricity_access_clustering$centers)
+center_cluster
+ggplot(electricty_access_country_year,aes(x = factor(Year), y = electricty_access_country_year$Access.to.electricity....of.population.,
+                              colour  = factor(electricity_access_clustering$cluster))) + geom_point() +
+geom_point(data = center_cluster, aes(x=factor(Year),y= center_cluster$Access.to.electricity....of.population.,colour  = 'Center')) + 
+geom_point(data = center_cluster, aes(x=factor(Year),y= center_cluster$Access.to.electricity....of.population.,colour = 'Center'), 
+ size=5.2,alpha=.3) + ylab("Access to electricity (% of population)") + xlab("Year")
+
+
+ggplot(electricty_access_country_year,aes(x = factor(CountryName), y = electricty_access_country_year$Access.to.electricity....of.population.,
+                                   colour  = factor(electricity_access_clustering$cluster))) + geom_point() 
+#+
+ # geom_point(data = center_cluster, aes(x=factor(Year),y= center_cluster$Access.to.electricity....of.population.,colour  = 'Center')) + 
+  #geom_point(data = center_cluster, aes(x=factor(Year),y= center_cluster$Access.to.electricity....of.population.,colour = 'Center'), 
+   #          size=5.2,alpha=.3) + ylab("Access to electricity (% of population)") + xlab("Year")
+as.data.frame(electricity_access_clustering$centers)
