42a50177a14610e64dfcecbb4378108bd123e5f0
niranjan272 <ndeshpande87@yahoo.com>
2016-11-17 17:37:22 -0600

    New Code
    Decsion Tree

---
+++ wdi_decision_tree.R
@@ -0,0 +1,13 @@
+setwd("E:\\Fall'16\\03 BUAN 6356\\Project\\Codes\\World-Bank-Indicator\\")
+library(ggplot2)
+library(cluster)
+library(plyr)
+library(fpc)
+
+#load hdi index file
+countries_hdi_index  <- read.csv("..\\..\\world-development-indicators\\hdi_index.csv")
+head(countries_hdi_index,5)
+count(countries_hdi_index,"hdi_index")
+
+indicator_hdi_index <- merge(indicator_pivot_continent,countries_hdi_index,by = "CountryName", all.x = T)
+                             
